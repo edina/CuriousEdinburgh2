93de79fa2f90450dae937b3ecac596406c5a7c6a
Jose Lloret <jose.lloret@ed.ac.uk>
2017-01-10 13:01:59 +0000

    #3. Create Map View on app home page

---
+++ index.ios.js
@@ -0,0 +1,5 @@
+
+import { AppRegistry } from 'react-native';
+import CuriousEdinburgh from './src/components/CuriousEdinburgh';
+
+AppRegistry.registerComponent('CuriousEdinburgh', () => CuriousEdinburgh);
