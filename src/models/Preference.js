3bae7028dd1769d8f4c62b392aa78f392b260519
Jose Lloret <jose.lloret@ed.ac.uk>
2017-02-02 15:34:29 +0000

    async storage for persisting tourId

---
+++ src/models/Preference.js
@@ -0,0 +1,21 @@
+import { AsyncStorage } from 'react-native';
+import * as constants from '../constants';
+
+export default class Preference {
+    static async getTourId() {
+        try {
+            const tourId = await AsyncStorage.getItem('tourId');
+            return tourId !== null ? tourId : constants.DEFAULT_TOUR_ID;
+        } catch (error) {
+            return constants.DEFAULT_TOUR_ID;
+        }
+    }
+    static async setTourId(value) {
+        try {
+            await AsyncStorage.setItem('tourId', value);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+}
