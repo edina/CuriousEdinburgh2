fb5d58a26cd0ba51f046acead68f41619e419e19
Jose Lloret <jose.lloret@ed.ac.uk>
2017-03-02 15:08:36 +0000

    Refactored componentDidMount, changeSelectedTour and immutable tours for CuriousEdinburgh component. #58

--- src/services/WordPress.js
+++ src/services/WordPress.js
@@ -1,29 +1,112 @@
+import Tour from '../models/Tour';
+import TourPlace from '../models/TourPlace';
+import Location from '../models/Location';
 import * as constants from '../constants';
+import Utils from '../utils';
+
+const Entities = require('html-entities').XmlEntities;
 /* global fetch:false*/
 export default class WordPress {
-    static getCategories() {
-        return fetch(constants.CATEGORIES, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-            } })
-          .then(response => response.json())
-          .then(responseJson =>
-              responseJson.filter(value => value.description === 'true'))
-          .catch((error) => {
-              console.log(error);
-          });
-    }
-    static getPostsFromCategory(value) {
-        return fetch(constants.POSTS_BY_CATEGORY + value, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-            } })
-        .then(response => response.json())
-        .then(responseJson => responseJson)
-        .catch((error) => {
-            console.log(error);
+    static getTours() {
+        const p1 = new Promise((resolve, reject) => {
+            fetch(constants.CATEGORIES, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+            }).then((onFullfilled) => {
+                if (onFullfilled.ok) {
+                    resolve(onFullfilled.json());
+                } else {
+                    reject({ status: onFullfilled.status,
+                        statusText: onFullfilled.statusText });
+                }
+            }, (onRejected) => {
+                reject({ statusText: onRejected.statusText });
+            });
         });
+        const p2 = new Promise((resolve, reject) => {
+            p1.then((data) => {
+                if (Array.isArray(data)) {
+                    const dataFiltered = data.filter(value => value.description === 'true');
+                    const tours = dataFiltered.map(value =>
+                        new Tour({ id: value.id ? value.id.toString() : '',
+                            name: value.name,
+                            description: value.description,
+                            slug: value.slug }));
+                    resolve(tours);
+                } else {
+                    reject({ statusText: 'An array is expected for the data retrieved' });
+                }
+                resolve(data);
+            }, (onRejected) => {
+                reject(onRejected);
+            });
+        });
+        return p2;
+    }
+    static getTourPlaces(tour) {
+        // TODO: Validate tour is instanceof Tour
+        const p1 = new Promise((resolve, reject) => {
+            console.log(constants.POSTS_BY_CATEGORY + tour.id);
+            fetch(constants.POSTS_BY_CATEGORY + tour.id, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+            }).then((onFullfilled) => {
+                if (onFullfilled.ok) {
+                    resolve(onFullfilled.json());
+                } else {
+                    reject({ status: onFullfilled.status,
+                        statusText: onFullfilled.statusText });
+                }
+            }, (onRejected) => {
+                reject({ statusText: onRejected.statusText });
+            });
+        });
+        const p2 = new Promise((resolve, reject) => {
+            p1.then((data) => {
+                if (Array.isArray(data)) {
+                    const entities = new Entities();
+                    const tourPlaces = data.map((value) => {
+                        const customFields = value.custom_fields;
+                        if (customFields) {
+                            let tourStop = null;
+                            try {
+                                tourStop = Utils.getTourStopFromSlug(
+                                    tour.slug, customFields.tour_stops);
+                            } catch (e) { console.log(e); }
+                            return new TourPlace({
+                                id: value.id ? value.id.toString() : '',
+                                title: customFields.OSM_Marker_01_Name,
+                                description: entities.decode(customFields.main_text),
+                                // TODO Check value returned from html-entities third party library
+                                images: Utils.getURLsFromHTMLImage(value.content.rendered),
+                                location: new Location({
+                                    latitude: Utils.toFloat(customFields.latitude),
+                                    longitude: Utils.toFloat(customFields.longitude) }),
+                                streetAddress: customFields.street_address,
+                                additionalLinks:
+                                    Utils.getURLsFromPipeString(customFields.additional_links),
+                                stop: tourStop,
+                                video: Utils.getEmbeddedYTURL(customFields.video_link),
+                            });
+                        }
+                        return new TourPlace({
+                            id: value.id ? value.id.toString() : '',
+                            images: Utils.getURLsFromHTMLImage(value.content.rendered) });
+                    });
+                    tourPlaces.sort((a, b) => a.stop - b.stop); // TODO check sorting places
+                    // when stop are not string (e.g null if regex failed)
+                    resolve(tourPlaces);
+                } else {
+                    reject({ statusText: 'An array is expected for the data retrieved' });
+                }
+            }, (onRejected) => {
+                reject(onRejected);
+            });
+        });
+        return p2;
     }
 }
--- src/utils.js
+++ src/utils.js
@@ -3,25 +3,41 @@ import { Platform } from 'react-native';
 export default class Utils {
     static getURLsFromHTMLImage(html) {
         const regex = /<img[^>]*(?:\bsrc\b\s*=)[^"]*"([^"]*)"[^>]*>/g;
-        return Utils.getFirstCapturedMatchForSuccessiveMatches(regex, html);
+        try {
+            return Utils.getFirstCapturedMatchForSuccessiveMatches(regex, html);
+        } catch (e) {
+            return [];
+        }
     }
     static getURLsFromPipeString(string) {
         const regex = /(\bhttp\b[^|]*|\bhttps\b[^|]*)/g;
-        return Utils.getFirstCapturedMatchForSuccessiveMatches(regex, string);
+        try {
+            return Utils.getFirstCapturedMatchForSuccessiveMatches(regex, string);
+        } catch (e) {
+            return [];
+        }
     }
     /*
         @param tourSlug represents the category slug (e.g. history_of_science) to match
-        @param tourStop represents the custom field containing all the tour stops
+        @param tourStops represent the custom field containing all the tour stops
             for a post (e.g. history_of_science:19|history_of_physics:1)
         @return the stop number for a given category slug or null if does not match
     */
-    static getTourStopFromSlug(tourSlug, tourStop) {
+    static getTourStopFromSlug(tourSlug, tourStops) {
+        if (!(typeof tourStops === 'string')) {
+            throw new TypeError('String parameter is expected for tourStops');
+        }
         const regex = new RegExp(`${tourSlug}:(\\d+)`, 'g');
-        const result = regex.exec(tourStop);
+        const result = regex.exec(tourStops);
         return result !== null ? result[1] : null;
     }
     static getFirstCapturedMatchForSuccessiveMatches(regex, string) {
-        // Types validation for parameter is recommended
+        if (!(regex instanceof RegExp)) {
+            throw new TypeError('RegExp parameter is expected for regex');
+        }
+        if (!(typeof string === 'string')) {
+            throw new TypeError('String parameter is expected for string');
+        }
         let matches = null;
         const result = [];
         while ((matches = regex.exec(string)) !== null) { // eslint-disable-line no-cond-assign
@@ -29,14 +45,12 @@ export default class Utils {
         }
         return result;
     }
-  
     static isIos() {
         return Platform.OS === 'ios';
     }
     static isAndroid() {
         return Platform.OS === 'android';
     }
-
     /**
      * Convert youtube page URL to a playable embedded URL.
      * @param Youtube page URL.
@@ -44,5 +58,12 @@ export default class Utils {
      */
     static getEmbeddedYTURL(url) {
         return url.replace('watch?v=', 'embed/');
+    }
+    static toFloat(value) {
+        const r = parseFloat(value);
+        if (!isFinite(r)) {
+            return 0.0;
+        }
+        return r;
     }
 }
