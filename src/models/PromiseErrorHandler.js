5906591b8baba395acbd85e3e112b4e9d36f3513
Jose Lloret <jose.lloret@ed.ac.uk>
2017-03-31 12:09:26 +0100

    removed URL from toString for security reasons. #76

--- src/models/PromiseErrorHandler.js
+++ src/models/PromiseErrorHandler.js
@@ -1,10 +1,10 @@
 export default class PromiseErrorHandler {
-    constructor({ status = 0, statusText = null, URL = null } = {}) {
+    constructor({ status = 0, statusText = 'unknown', URL = null } = {}) {
         this.status = status;
         this.statusText = statusText;
         this.URL = URL;
     }
     toString() {
-        return `{ status: ${this.status}, statusText: ${this.statusText}, URL: ${this.URL} }`;
+        return `{ status: ${this.status}, statusText: ${this.statusText} }`;
     }
 }
