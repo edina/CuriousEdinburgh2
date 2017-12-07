f517827d95ce67dbab851da0975f81c2ddd28e57
Jose Lloret <jose.lloret@ed.ac.uk>
2017-12-07 12:21:06 +0000

    underdayColor for tourplace links set to transparent. #81
    header image with flex property. #81

--- src/components/TourRecord.js
+++ src/components/TourRecord.js
@@ -61,6 +61,7 @@ export default class TourRecord extends Component {
           <TouchableHighlight
             key={link.url}
             onPress={() => Linking.openURL(link.url)}
+            underlayColor="transparent"
           >
             <Text style={styles.link}>{link.text}</Text>
           </TouchableHighlight>,
