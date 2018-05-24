32cebffe9a996ca732170265019bc6d6eeea18ac
Adrian Czuczka <adrianczuczka@gmail.com>
2018-05-24 11:17:42 +0100

    Changed the style of the TourPlaceList, specifically the numbering, to better fit the space.

--- src/components/TourPlaceList.js
+++ src/components/TourPlaceList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Image,
-         ListView,
-         Text,
-         TouchableHighlight,
-         View } from 'react-native';
-import TourRecord from './TourRecord';
-import styles from './styles/TourPlaceList';
+import {
+    Image,
+    ListView,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import TourRecord from '../components/TourRecord';
+import styles from '../components/styles/TourPlaceList';
 
 export default class TourPlaceList extends Component {
     constructor(props) {
@@ -41,36 +43,41 @@ export default class TourPlaceList extends Component {
         return (
           <View style={styles.page}>
             <TourRecord
-              ref={(c) => { this.modal = c; }}
+              ref={(c) => {
+                  this.modal = c;
+              }}
             />
             <ListView
               dataSource={this.state.dataSource}
               renderRow={
-                rowData =>
-                  <TouchableHighlight
-                    onPress={
-                      () => this._onPressRecord(rowData)
+                        rowData =>
+                          <View style={styles.place}>
+                            <TouchableOpacity
+                              onPress={
+                                        () => this._onPressRecord(rowData)
+                                    }
+                              activeOpacity={1}
+                            >
+                              <View style={styles.container}>
+                                <View style={styles.numberContainer}>
+                                  <Text style={styles.number}>{rowData.stop}</Text>
+                                </View>
+                                <Text style={styles.title}>{rowData.title}</Text>
+                              </View>
+                              <View style={styles.detail}>
+                                <Image
+                                  style={styles.image}
+                                  source={{ uri: rowData.images[0].url }}
+                                />
+                                <Text
+                                  style={styles.text}
+                                >
+                                  {rowData.getShortDescription()}
+                                </Text>
+                              </View>
+                            </TouchableOpacity>
+                          </View>
                     }
-                  >
-                    <View style={styles.place}>
-                      <Text style={styles.title}>{rowData.title}</Text>
-                      <View style={styles.detail}>
-                        <View style={styles.numberContainer}>
-                          <Text style={styles.number}>{rowData.stop}</Text>
-                        </View>
-                        <Image
-                          style={styles.image}
-                          source={{ uri: rowData.images[0].url }}
-                        />
-                        <Text
-                          style={styles.text}
-                        >
-                          {rowData.getShortDescription()}
-                        </Text>
-                      </View>
-                    </View>
-                  </TouchableHighlight>
-              }
             />
           </View>
         );
